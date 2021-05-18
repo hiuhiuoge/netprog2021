b53bb7831bc8019ead8fc328f96b7c5cbedff326
hiuhiuoge <70254587+hiuhiuoge@users.noreply.github.com>
2021-05-18 10:16:56 +0700

    Update 05.practical.work.client.turn.c

--- 05.practical.work.client.turn.c
+++ 05.practical.work.client.turn.c
@@ -57,7 +57,7 @@ int main(int argc, char *argv)
 
     valread = read( new_socket , buffer, 1024);
     printf("%s\n",buffer );
-    send(new_socket , hello , strlen(hello) , 0 );
-    printf("Hello message sent\n");
+    send(new_socket , buffer , strlen(buffer) , 0 );
+    printf("Message sent\n");
     return 0;
 }
