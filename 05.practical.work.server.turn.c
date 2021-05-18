3bafa4fd587bb9221c46573aa5d94ce7b7ed0e70
hiuhiuoge <phamanhhieu1502@gmail.com>
2021-05-17 19:10:53 -0700

    05.practical.work.turn

---
+++ 05.practical.work.server.turn.c
@@ -0,0 +1,44 @@
+05.practical.work.server.turn.c 
+#include <stdio.h>
+#include <sys/types.h> 
+#include <sys/socket.h>
+#include <netinet/in.h>
+
+int main (int argc, char *argv[])
+{
+    int sockfd, clen, clientfd;
+    struct sockaddr_in saddr, caddr, cli_addr;
+    unsigned short port = 8784;
+
+    if ((sockfd=socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+        printf(“Error creating socket\n”);
+        return -1;
+    }
+    memset(&saddr, 0, sizeof(saddr));
+    saddr.sin_family = AF_INET;
+    saddr.sin_addr.s_addr = htonl(INADDR_ANY);
+    saddr.sin_port = htons(port);
+    
+    if ((bind(sockfd, (struct sockaddr *) &saddr, sizeof(saddr)) < 0) {
+        printf(“Error binding\n”);
+        return -1;
+    }
+
+    if (listen(sockfd, 5) < 0) {
+        printf(“Error listening\n”);
+        return -1;
+    }
+
+    clen=sizeof(caddr);
+
+    if ((clientfd=accept(sockfd, (struct sockaddr *) &caddr, &clen)) < 0) {
+        printf(“Error accepting connection\n”);
+        return -1
+    }
+
+    FILE *f;
+    f=fopen("add.txt","r");
+    fscanf(f,"%s",buf);
+    write(sockfd,buf,200);
+    printf("the file was sent successfully");
+}
